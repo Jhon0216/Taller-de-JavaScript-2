4f5bf83433e5c6d256e92d631dd82fd3fb67fe1f
John Alexander Florez <78865261+Jhon0216@users.noreply.github.com>
2021-03-16 20:21:22 -0500

    Ejercicio #4
    Ejercicio #4 de Taller de JavaScript 2

---
+++ EJERCICIO #4/Scripts/mensaje.js
@@ -0,0 +1,23 @@
+function Boton(miboton){
+	miboton.innerHTML = "<strong>Hugo</strong>";
+}
+
+function Boton2(miboton){
+	miboton.innerHTML = "Hugo";
+}
+
+function Boton3(miboton){
+	miboton.innerHTML = "<strong>Paco</strong>";
+}
+
+function Boton4(miboton){
+	miboton.innerHTML = "Paco";
+}
+
+function Boton5(miboton){
+	miboton.innerHTML = "<strong>Luis</strong>";
+}
+
+function Boton6(miboton){
+	miboton.innerHTML = "Luis";
+}
