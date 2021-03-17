58cfd840cd51b8d0905621fbaa72a60546de3d28
John Alexander Florez <78865261+Jhon0216@users.noreply.github.com>
2021-03-16 20:18:55 -0500

    Ejercicio #3
    Ejercicio #3 de Taller de JavaScript 2

---
+++ EJERCICIO #3/Scripts/mensaje.js
@@ -0,0 +1,13 @@
+function Mensaje(){
+	let miDidDeInteres = document.getElementById("contenido");
+	miDidDeInteres.style.backgroundColor = "black";
+	
+	let divsACambiar = document.getElementsByClassName("mascontenido");
+	for (let i = o; i < DivCambiar.length; i++){
+		divsACambiar[i].style.backgroundColor = "black";
+	}
+}
+
+function boton(miboton){
+	miboton.innerHTML = "Hicieron click aqui";
+}
